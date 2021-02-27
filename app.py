9ffa1da9039e703963d7a976169be20440bf0d7d
Quoc Anh Truong <quocanhkma1999@gmail.com>
2021-02-27 15:05:34 +0700

    add jwt_extended and UserLogin resource

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 
 from flask_jwt_extended import JWTManager
 
-
 from resources.user import UserRegister, User, UserLogin
 
 from resources.item import Item, ItemList
@@ -18,13 +17,6 @@ app.secret_key = "teomoney"
 
 api = Api(app)
 
-# Authentication endpoint
-app.config["JWT_AUTH_URL_RULE"] = "/login"
-# Token expiration time 
-app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=1800)
-# Authentication key name 
-app.config["JWT_AUTH_USERNAME_KEY"] = "username"
-
 # SQLAlchemy Configuration
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -32,20 +24,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:/
 # "postgres://bhbshkidakkphb:d24fb10fa3c4e44123ebcf7b24b00cb38f8cdbb0658c834c94f6ba27b9744f64@ec2-3-213-85-90.compute-1.amazonaws.com:5432/d5olmhl87fmspp"
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-@jwt.jwt_error_handler
-def customized_error_handler(error): 
-    return jsonify({
-        "message": error.description, 
-        "code": error.status_code
-    }), error.status_code
 
-
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity): 
-    return jsonify({
-        "access_token": access_token.decode("utf-8"), 
-        "user_id": identity.id
-    })
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWTManager(app)
 
